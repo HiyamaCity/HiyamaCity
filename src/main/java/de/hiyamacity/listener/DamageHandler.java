6091623fb369c00f21e43ab3028cc01d79555e8c
AhegaHOE <kratzeisarmy@gmail.com>
2022-03-30 21:45:20 +0200

    Initial Commit

--- src/main/java/de/hiyamacity/listener/DamageHandler.java
+++ src/main/java/de/hiyamacity/listener/DamageHandler.java
@@ -1,13 +1,9 @@
 package de.hiyamacity.listener;
 
-import de.hiyamacity.items.Enchantments;
-import de.hiyamacity.items.Katana;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
-import java.util.Objects;
 
 public class DamageHandler implements Listener {
 
