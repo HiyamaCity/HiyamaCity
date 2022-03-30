c8eaed088a0bdeb48a341f5290063ff438be1b94
Ina <abelinafleischmann2302@gmail.com>
2022-03-31 01:23:53 +0200

    Changes by AhegaHOE

--- src/main/java/de/hiyamacity/misc/Distances.java
+++ src/main/java/de/hiyamacity/misc/Distances.java
@@ -1,10 +1,9 @@
 package de.hiyamacity.misc;
 
-public enum Distances {
+public interface Distances {
 
-    CHAT_MESSAGE_NEAREST(8 ^ 2), CHAT_MESSAGE_NORMAL(16 ^ 2), CHAT_MESSAGE_FURTHEST(32 ^ 2);
+    int CHAT_MESSAGE_NEAREST = 8 ^ 2;
+    int CHAT_MESSAGE_NORMAL = 16 ^ 2;
+    int CHAT_MESSAGE_FURTHEST = 32 ^ 2;
 
-    Distances(int i) {
-
-    }
 }
