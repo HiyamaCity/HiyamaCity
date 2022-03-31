750f0a8ff15d6bae574d2c5401b71c97eae16170
AhegaHOE <kratzeisarmy@gmail.com>
2022-03-31 18:03:47 +0200

    / PlaytimeTracker; now working as intended

--- src/main/java/de/hiyamacity/util/PlaytimeTracker.java
+++ src/main/java/de/hiyamacity/util/PlaytimeTracker.java
@@ -5,26 +5,36 @@ import de.hiyamacity.main.Main;
 import de.hiyamacity.objects.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlaytimeTracker {
 
     public static void startPlaytimeTracker() {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getInstance(), () -> {
-            Bukkit.getOnlinePlayers().forEach(player -> {
-                User user = MySqlPointer.getUser(player.getUniqueId());
-                long minutes = user.getPlayedMinutes();
-                long hours = user.getPlayedHours();
 
-                minutes++;
-                user.setPlayedMinutes(minutes);
-                if (minutes >= 60) {
-                    minutes = 0;
-                    hours++;
-                    user.setPlayedHours(hours);
-                }
-                MySqlPointer.updateUser(player.getUniqueId(), user);
-            });
-        }, 20 * 60, 20 * 60);
+        BukkitRunnable runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    User user = MySqlPointer.getUser(player.getUniqueId());
+                    long minutes = user.getPlayedMinutes();
+                    long hours = user.getPlayedHours();
+
+                    minutes++;
+                    user.setPlayedMinutes(minutes);
+                    if (minutes >= 60) {
+                        minutes = 0;
+                        hours++;
+                        user.setPlayedMinutes(minutes);
+                        user.setPlayedHours(hours);
+                    }
+                    MySqlPointer.updateUser(player.getUniqueId(), user);
+
+                });
+            }
+        };
+
+        runnable.runTaskTimer(Main.getInstance(), 20 * 60, 20 * 60);
+
     }
 
 }
