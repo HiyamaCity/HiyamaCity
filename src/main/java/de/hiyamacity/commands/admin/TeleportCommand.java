540fb4a6c0de798cc88878a96d3d62719075be87
AhegaHOE <kratzeisarmy@gmail.com>
2022-03-31 20:26:23 +0200

    + TeleportCommand

---
+++ src/main/java/de/hiyamacity/commands/admin/TeleportCommand.java
@@ -0,0 +1,15 @@
+package de.hiyamacity.commands.admin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class TeleportCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        return false;
+    }
+}
