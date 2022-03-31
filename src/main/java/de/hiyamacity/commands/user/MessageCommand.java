a7b39560aa6fbc43aee5ccb4da9b6a2b56a5de26
AhegaHOE <kratzeisarmy@gmail.com>
2022-03-31 14:50:05 +0200

    / Syntax revision on MessageCommand

--- src/main/java/de/hiyamacity/commands/user/MessageCommand.java
+++ src/main/java/de/hiyamacity/commands/user/MessageCommand.java
@@ -12,14 +12,13 @@ import java.util.ResourceBundle;
 public class MessageCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender Sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (!(Sender instanceof Player)) {
-            Player p = (Player) Sender;
-            ResourceBundle resourceBundle = LanguageHandler.getResourceBundle(p.getUniqueId());
-            return true;
-        }
-        // Ich weiß nicht genau wie ich das machen muss, wenn er quasi akzeptieren soll das es auch mehr als 2 Stellen geben kann
-        if ( args.length !=2){
-            p.sendmessage(resourceBundle.getString("messageUsage"));
+        if (!(Sender instanceof Player)) return true;
+
+        Player p = (Player) Sender;
+        ResourceBundle resourceBundle = LanguageHandler.getResourceBundle(p.getUniqueId());
+
+        if (args.length != 2) {
+            p.sendMessage(resourceBundle.getString("messageUsage"));
             return true;
         }
 
