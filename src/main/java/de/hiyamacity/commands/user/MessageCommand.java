e71228b62d286c99981f83a1adc3b8cc6aa0929d
Ina <abelinafleischmann2302@gmail.com>
2022-03-31 13:12:26 +0200

    MessageCommand hinzugefügt

---
+++ src/main/java/de/hiyamacity/commands/user/MessageCommand.java
@@ -0,0 +1,20 @@
+package de.hiyamacity.commands.user;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender Sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (!( Sender instanceof Player)) {
+            return true;
+        }
+    Player p = (Player) sender;
+
+
+
+
+}
