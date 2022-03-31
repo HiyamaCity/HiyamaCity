eb4e41472360756a6276a06d7128eff7ade8db68
Leon <leonreitz04@gmail.com>
2022-03-31 16:58:22 +0200

    InfoCommand hinzugefügt

---
+++ src/main/java/de/hiyamacity/commands/user/InfoCommand.java
@@ -0,0 +1,24 @@
+package de.hiyamacity.commands.user;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class InfoCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        if (!(sender instanceof Player)) return true;
+
+
+        Player p = (Player) sender;
+
+        p.sendMessage("ich bin der banger");
+
+
+        return false;
+    }
+}
