9defd4a97a2dfb1805ce99a53b1673284ca88158
AhegaHOE <kratzeisarmy@gmail.com>
2022-03-31 20:36:51 +0200

    / TeleportCommand

--- src/main/java/de/hiyamacity/commands/admin/TeleportCommand.java
+++ src/main/java/de/hiyamacity/commands/admin/TeleportCommand.java
@@ -7,7 +7,6 @@ import org.jetbrains.annotations.NotNull;
 
 public class TeleportCommand implements CommandExecutor {
 
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         return false;
