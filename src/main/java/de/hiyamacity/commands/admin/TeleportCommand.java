31e92a3007d9fdcd3265133529a8d416158c2511
AhegaHOE <kratzeisarmy@gmail.com>
2022-04-01 14:38:31 +0200

    / TeleportCommand

--- src/main/java/de/hiyamacity/commands/admin/TeleportCommand.java
+++ src/main/java/de/hiyamacity/commands/admin/TeleportCommand.java
@@ -9,6 +9,7 @@ public class TeleportCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
         return false;
     }
 }
