c92fc43d839a1a67a64d88eadcc5814f95ebaee6
Ina <abelinafleischmann2302@gmail.com>
2022-04-04 22:11:41 +0200

    SlapCommand kleine Änderungen nicht fertig

--- src/main/java/de/hiyamacity/commands/user/SlapCommand.java
+++ src/main/java/de/hiyamacity/commands/user/SlapCommand.java
@@ -34,18 +34,17 @@ public class SlapCommand implements CommandExecutor {
             return true;
         }
         if (p.getName().equals(t.getName())) {
-            p.sendMessage(rs.getString(""));
+            p.sendMessage(rs.getString("slapSelf"));
             return true;
         }
         Location pLoc = new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY() + 2, p.getLocation().getZ());
         Location tLoc = new Location(t.getWorld(), t.getLocation().getX(), t.getLocation().getY() + 2, t.getLocation().getZ());
 
         for (Player o : Bukkit.getOnlinePlayers()) {
-            o.spawnParticle(Particle.HEART, pLoc, 1);
-            o.spawnParticle(Particle.HEART, tLoc, 1);
+
             if (p.getLocation().distanceSquared(t.getLocation()) <= Distances.KISS) {
                 ResourceBundle ors = LanguageHandler.getResourceBundle(o.getUniqueId());
-                o.sendMessage(ors.getString("kiss").replace("%player%", p.getName()).replace("%target%", t.getName()));
+                o.sendMessage(ors.getString("slap").replace("%player%", p.getName()).replace("%target%", t.getName()));
             }
         }
 
