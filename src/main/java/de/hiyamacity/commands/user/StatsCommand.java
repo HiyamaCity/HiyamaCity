8a49f3f51836f53c796ea6d9719790a67c51918f
AhegaHOE <kratzeisarmy@gmail.com>
2022-04-14 00:14:21 +0200

    TeleportCommand onTabComplete changes + StatsCommand for other users

--- src/main/java/de/hiyamacity/commands/user/StatsCommand.java
+++ src/main/java/de/hiyamacity/commands/user/StatsCommand.java
@@ -4,6 +4,7 @@ import de.hiyamacity.database.MySqlPointer;
 import de.hiyamacity.objects.User;
 import de.hiyamacity.util.DecimalSeparator;
 import de.hiyamacity.lang.LanguageHandler;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,29 +12,60 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
+import java.util.ResourceBundle;
 
 public class StatsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (!(sender instanceof Player p)) return true;
+        ResourceBundle rs = LanguageHandler.getResourceBundle(p.getUniqueId());
+        // TODO: Stats von anderen Spielern sehen.
+        switch (args.length) {
+            case 1 -> {
+                User user = MySqlPointer.getUser(p.getUniqueId());
+                long hours = user.getPlayedHours();
+                long minutes = user.getPlayedMinutes();
+                long money = user.getPurse();
+                long bank = user.getBank();
 
+                DecimalFormat decimalFormat = DecimalSeparator.prepareFormat(',', '.', false, (byte) 0);
 
-        User user = MySqlPointer.getUser(p.getUniqueId());
-        long hours = user.getPlayedHours();
-        long minutes = user.getPlayedMinutes();
-        long money = user.getPurse();
-        long bank = user.getBank();
+                String msg = LanguageHandler.getResourceBundle(p.getUniqueId()).getString("statsMessage")
+                        .replace("%target%", p.getName())
+                        .replace("%hours%", decimalFormat.format(hours))
+                        .replace("%minutes%", decimalFormat.format(minutes))
+                        .replace("%money%", decimalFormat.format(money))
+                        .replace("%bank%", decimalFormat.format(bank));
+                p.sendMessage(msg);
+            }
+            case 2 -> {
+                if (!p.hasPermission("statsOther")) return true;
+                Player t = Bukkit.getPlayer(args[0]);
+                if (t == null) {
+                    p.sendMessage(rs.getString("playerNotFound"));
+                    return true;
+                }
+                User user = MySqlPointer.getUser(t.getUniqueId());
+                long hours = user.getPlayedHours();
+                long minutes = user.getPlayedMinutes();
+                long money = user.getPurse();
+                long bank = user.getBank();
 
-        DecimalFormat decimalFormat = DecimalSeparator.prepareFormat(',', '.', false, (byte) 0);
+                DecimalFormat decimalFormat = DecimalSeparator.prepareFormat(',', '.', false, (byte) 0);
 
-        String msg = LanguageHandler.getResourceBundle(p.getUniqueId()).getString("statsMessage")
-                .replace("%target%", p.getName())
-                .replace("%hours%", decimalFormat.format(hours))
-                .replace("%minutes%", decimalFormat.format(minutes))
-                .replace("%money%", decimalFormat.format(money))
-                .replace("%bank%", decimalFormat.format(bank));
-        p.sendMessage(msg);
+                String msg = LanguageHandler.getResourceBundle(p.getUniqueId()).getString("statsMessage")
+                        .replace("%target%", t.getName())
+                        .replace("%hours%", decimalFormat.format(hours))
+                        .replace("%minutes%", decimalFormat.format(minutes))
+                        .replace("%money%", decimalFormat.format(money))
+                        .replace("%bank%", decimalFormat.format(bank));
+                p.sendMessage(msg);
+            }
+            default -> {
+                return true;
+            }
+        }
         return false;
     }
 }
