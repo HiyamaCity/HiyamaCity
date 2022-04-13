7d3fa45753d3a6f8359b83eb433696837c3bbbbc
Leon <leonreitz04@gmail.com>
2022-04-14 01:36:17 +0200

    heal und kick command hinzugefügt

--- src/main/java/de/hiyamacity/commands/admin/KickCommand.java
+++ src/main/java/de/hiyamacity/commands/admin/KickCommand.java
@@ -3,7 +3,10 @@ package de.hiyamacity.commands.admin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ResourceBundle;
 
 public class KickCommand implements CommandExecutor {
 
--- src/main/java/de/hiyamacity/commands/user/HealCommand.java
+++ src/main/java/de/hiyamacity/commands/user/HealCommand.java
@@ -3,7 +3,10 @@ package de.hiyamacity.commands.user;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ResourceBundle;
 
 public class HealCommand implements CommandExecutor {
 
