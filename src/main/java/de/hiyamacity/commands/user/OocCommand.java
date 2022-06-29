d0ad319e7bd0dcdfff570a13f6e255cd6daf4f13
AhegaHOE <kratzeisarmy@gmail.com>
2022-06-29 16:18:11 +0200

    Bare Bones for #7

---
+++ src/main/java/de/hiyamacity/commands/user/OocCommand.java
@@ -0,0 +1,16 @@
+package de.hiyamacity.commands.user;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class OocCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        //TODO: FINISH
+        return false;
+    }
+}
