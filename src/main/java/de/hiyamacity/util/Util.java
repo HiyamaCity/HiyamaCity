88d174c4f59119ebb7ad22c9a84bf0a3581fe618
AhegaHOE <kratzeisarmy@gmail.com>
2023-03-24 08:12:08 +0100

    converted util class to interface

--- src/main/java/de/hiyamacity/util/Util.java
+++ src/main/java/de/hiyamacity/util/Util.java
@@ -10,9 +10,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
-public class Util {
+public interface Util {
 
-	public static boolean isLong(String s) {
+	static boolean isLong(String s) {
 		try {
 			Long.parseLong(s);
 			return true;
@@ -20,13 +20,22 @@ public class Util {
 			return false;
 		}
 	}
-	
-	public static Optional<Location> getOpenableLocation(@NotNull Block block) {
+
+	/**
+	 * This method returns just the Location of the Block that it was given if it is not a door.
+	 * If the Block is an instance of a door we check whether the upper part or lower part was given.
+	 * When the upper part of the door was given this method subtracts 1 from the Y coordinate of the given Blocks location.
+	 *
+	 * @param block The Block that is looked at
+	 *
+	 * @return The determined location
+	 */
+	static Optional<Location> getOpenableLocation(@NotNull Block block) {
 		final BlockData blockData = block.getBlockData();
-		if(!(blockData instanceof Openable)) return Optional.empty();
-		if(blockData instanceof final Door door) {
+		if (!(blockData instanceof Openable)) return Optional.empty();
+		if (blockData instanceof final Door door) {
 			final Bisected.Half half = door.getHalf();
-			if(half == Bisected.Half.TOP) {
+			if (half == Bisected.Half.TOP) {
 				return Optional.of(block.getLocation().subtract(0, 1, 0));
 			} else {
 				return Optional.of(block.getLocation());
